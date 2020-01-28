d6d6e2cdda28aa57728887263400937adb5d6534
Sebastian <seba.torrico5@gmail.com>
2020-01-28 15:26:46 -0300

    class name change, whitespace are removed and constant modifier is added the error variable

--- src/com/company/FibonacciExecutor.java
+++ src/com/company/FibonacciExecutor.java
@@ -1,27 +1,20 @@
 package com.company;
 
+public class FibonacciExecutor {
 
-public class FibonacciExecute {
-
-    public static String ERROR_CODE = "Error no negative numbers are accepted\n";
+    public static final String ERROR_CODE = "Error no negative numbers are accepted";
 
     public static int execute(int fibonacciCardinal) {
-
 
         if (fibonacciCardinal < 0) {
             return Integer.parseInt(ERROR_CODE);
         }
-
-        if (fibonacciCardinal > 0 && fibonacciCardinal <= 2) {
+        else if (fibonacciCardinal > 0 && fibonacciCardinal <= 2) {
             return 1;
         } else {
             return execute(fibonacciCardinal - 1) + execute(fibonacciCardinal - 2);
         }
-
-
     }
-
-
 }
 
 
